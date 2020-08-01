7d7ad3f56ddce8a76f3a2911d5dd3fe2ad35679e
shihas <shihasbacker@yahoo.com>
2020-08-01 12:52:25 +0530

    username and password given

--- kseb-login-billpay.py
+++ kseb-login-billpay.py
@@ -3,3 +3,9 @@ from selenium import webdriver
 browser = webdriver.Firefox(executable_path="///home/shihas/Downloads/geckodriver-v0.27.0-linux64/geckodriver")
 
 browser.get("https://wss.kseb.in/selfservices/wssloginUser.do")
+
+user_name = browser.find_element_by_xpath('//*[@id="appendedtext1"]')
+user_name.send_keys('username')
+
+passWord = browser.find_element_by_xpath('//*[@id="appendedtext2"]')
+passWord.send_keys('password')
