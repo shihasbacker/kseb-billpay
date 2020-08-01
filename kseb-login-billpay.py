5e5f989da06caf5a38794413f0242eaa383e5933
shihas <shihasbacker@yahoo.com>
2020-08-01 12:51:37 +0530

    website loaded

--- kseb-login-billpay.py
+++ kseb-login-billpay.py
@@ -1,3 +1,5 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path="///home/shihas/Downloads/geckodriver-v0.27.0-linux64/geckodriver")
+
+browser.get("https://wss.kseb.in/selfservices/wssloginUser.do")
